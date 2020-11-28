5c2f2da4f81f7edebfb7cee8886558a82f8e7700
Lennart K <18233294+lennart-k@users.noreply.github.com>
2020-11-28 17:46:55 +0100

    Fixing navigation routing

--- src/sw.js
+++ src/sw.js
@@ -1,31 +1,30 @@
-import { registerRoute, NavigationRoute } from 'workbox-routing'
+import { registerRoute, NavigationRoute, RegExpRoute } from 'workbox-routing'
 import { precacheAndRoute, createHandlerBoundToURL, precache } from 'workbox-precaching'
 import * as strategies from 'workbox-strategies'
 import * as workbox from 'workbox-core'
 
 const manifest = self.__WB_MANIFEST
-const NAVIGATION_ROUTE = '/frontend/index.html'
+const INDEX_ROUTE = '/frontend/index.html'
 
 self.addEventListener('activate', () => {
   console.log(manifest)
 })
 
-precache([{ url: NAVIGATION_ROUTE }])
+precache([{ url: INDEX_ROUTE }])
 
 registerRoute(
-  new RegExp(/\/frontend\/index.html/),
-  new strategies.StaleWhileRevalidate())
-
-console.log(self.__WB_MANIFEST)
+  new RegExp(INDEX_ROUTE),
+  new strategies.StaleWhileRevalidate()
+)
 
 precacheAndRoute(manifest)
 
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL(NAVIGATION_ROUTE, {
-    allowlist: [new RegExp(/frontend/)],
-    denylist: [new RegExp(/frontend\/static/)]
-  })
-))
+console.log(manifest)
+
+const indexHandler = createHandlerBoundToURL(INDEX_ROUTE)
+const indexRoute = new RegExpRoute(/\/frontend(?!\/static).*/, indexHandler)
+registerRoute(indexRoute)
+
 
 self.addEventListener("message", (message) => {
   if (message.data.type === "skipWaiting") {
