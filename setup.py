8661432f789e6564ab739dff17b0d43f0051907f
Lennart K <18233294+lennart-k@users.noreply.github.com>
2020-12-13 18:51:12 +0100

    Bumped version to 20201213.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="homecontrol_frontend",
-    version="20201113.1",
+    version="20201213.0",
     url="https://github.com/lennart-k/HomeControl-Frontend",
     author="Lennart K",
     description="The frontend for HomeControl",
